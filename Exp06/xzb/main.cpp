1ab744da2320141f1c064cb0ec32c5c3a305c957
gudy <1242018755@qq.com>
2019-01-04 22:54:55 +0800

    Update main.cpp

--- Exp06/xzb/main.cpp
+++ Exp06/xzb/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #include "./gdal/gdal_priv.h"
 #pragma comment(lib, "gdal_i.lib")
 void dividedByBlock(int tx,int ty,int imgXlen,int imgYlen,GDALDataset* mulPic,GDALDataset* panPic,GDALDataset* outPic,float* buffR,float* buffG,float* buffB,float* buffP,float* buffH,float* buffS){
-    //分块处理转换
+	//分块处理转换
 	for(int i = 0;i < imgXlen;i+=256){
         for(int j = 0 ; j < imgYlen;j+=256){
             if(i + ty > imgXlen || j + tx > imgYlen)
@@ -35,40 +35,40 @@ void dividedByBlock(int tx,int ty,int imgXlen,int imgYlen,GDALDataset* mulPic,GD
 //分行处理
 void dividedByLine(int tx,int ty,int imgXlen, GDALDataset* mulPic,GDALDataset* panPic,GDALDataset* outPic,float* buffR,float* buffG,float* buffB,float* buffP,float* buffH,float* buffS){
 
-    //分行处理  256行一组
-    for(int i = 0 ; i < imgXlen;i+=256){
-        printf("图像的x:%d  y:%d\n",i,0);
-        if(i + tx > imgXlen)
-            break;
-        //读入缓存
-        mulPic->GetRasterBand(1)->RasterIO(GF_Read,i,0,tx,ty,buffR,tx,ty,GDT_Float32,0,0);
-        mulPic->GetRasterBand(2)->RasterIO(GF_Read,i,0,tx,ty,buffG,tx,ty,GDT_Float32,0,0);
-        mulPic->GetRasterBand(3)->RasterIO(GF_Read,i,0,tx,ty,buffB,tx,ty,GDT_Float32,0,0);
-        panPic->GetRasterBand(1)->RasterIO(GF_Read,i,0,tx,ty,buffP,tx,ty,GDT_Float32,0,0);
-        //转换处理
-        for(int k = 0;k < tx * ty ;k++){
-            buffH[k] = -sqrt(2.0f)/6.0f * buffR[k] - sqrt(2.0f)/6.0f*buffG[k] + sqrt(2.0f)/3.0f*buffB[k];
-            buffS[k] = 1.0f/sqrt(2.0f)*buffR[k] - 1/sqrt(2.0f)*buffG[k];
+	//分行处理  256行一组
+	for(int i = 0 ; i < imgXlen;i+=256){
+		printf("图像的x:%d  y:%d\n",i,0);
+		if(i + tx > imgXlen)
+		    break;
+		//读入缓存
+		mulPic->GetRasterBand(1)->RasterIO(GF_Read,i,0,tx,ty,buffR,tx,ty,GDT_Float32,0,0);
+		mulPic->GetRasterBand(2)->RasterIO(GF_Read,i,0,tx,ty,buffG,tx,ty,GDT_Float32,0,0);
+		mulPic->GetRasterBand(3)->RasterIO(GF_Read,i,0,tx,ty,buffB,tx,ty,GDT_Float32,0,0);
+		panPic->GetRasterBand(1)->RasterIO(GF_Read,i,0,tx,ty,buffP,tx,ty,GDT_Float32,0,0);
+		//转换处理
+		for(int k = 0;k < tx * ty ;k++){
+		    buffH[k] = -sqrt(2.0f)/6.0f * buffR[k] - sqrt(2.0f)/6.0f*buffG[k] + sqrt(2.0f)/3.0f*buffB[k];
+		    buffS[k] = 1.0f/sqrt(2.0f)*buffR[k] - 1/sqrt(2.0f)*buffG[k];
 
-            buffR[k] = buffP[k] - 1.0f/sqrt(2.0f) * buffH[k] + 1.0f/sqrt(2.0f) * buffS[k];
-            buffG[k] = buffP[k] - 1.0f/sqrt(2.0f) * buffH[k] - 1.0f/sqrt(2.0f) * buffS[k];
-            buffB[k] = buffP[k] + sqrt(2.0f) * buffH[k];
-        }
-        //写入新图像
-        outPic->GetRasterBand(1)->RasterIO(GF_Write,i,0,tx,ty,buffR,tx,ty,GDT_Float32,0,0);
-        outPic->GetRasterBand(2)->RasterIO(GF_Write,i,0,tx,ty,buffG,tx,ty,GDT_Float32,0,0);
-        outPic->GetRasterBand(3)->RasterIO(GF_Write,i,0,tx,ty,buffB,tx,ty,GDT_Float32,0,0);
-    }
+		    buffR[k] = buffP[k] - 1.0f/sqrt(2.0f) * buffH[k] + 1.0f/sqrt(2.0f) * buffS[k];
+		    buffG[k] = buffP[k] - 1.0f/sqrt(2.0f) * buffH[k] - 1.0f/sqrt(2.0f) * buffS[k];
+		    buffB[k] = buffP[k] + sqrt(2.0f) * buffH[k];
+		}
+		//写入新图像
+		outPic->GetRasterBand(1)->RasterIO(GF_Write,i,0,tx,ty,buffR,tx,ty,GDT_Float32,0,0);
+		outPic->GetRasterBand(2)->RasterIO(GF_Write,i,0,tx,ty,buffG,tx,ty,GDT_Float32,0,0);
+		outPic->GetRasterBand(3)->RasterIO(GF_Write,i,0,tx,ty,buffB,tx,ty,GDT_Float32,0,0);
+  	 }
 }
 int main()
 {
-    char* mulPath = "Mul_large.tif";
-    char* panPath = "Pan_large.tif";
-    //图像的宽度和高度
-	int imgXlen, imgYlen;
+	char* mulPath = "Mul_large.tif";
+	char* panPath = "Pan_large.tif";
+	//图像的宽度和高度
+	int imgXlen, imgYlen; 
 	//图像波段数
 	int bandNum;
-    //输入图像
+	//输入图像
 	GDALDataset* mulPic,*panPic;
 	//输出图像
 	GDALDataset* outPic;
@@ -91,46 +91,46 @@ int main()
 	cout << "Band Number:" << bandNum << endl;
 	int tx = 256,ty,choose;
 	//输出提示
-    cout << "you can choose the way to create the picture" << endl;
-    cout << "1、 divided by line" << endl;
-    cout << "2、 divided by block" << endl;
-    cin >> choose;
-    if(choose == 1){
-        ty = imgYlen;
-    }else{
-        ty = 256;
-    }
-    buffR = (float *)CPLMalloc(tx * ty * sizeof(float));
-    buffG = (float *)CPLMalloc(tx * ty * sizeof(float));
-    buffB = (float *)CPLMalloc(tx * ty * sizeof(float));
-    buffP = (float *)CPLMalloc(tx * ty * sizeof(float));
-    buffH = (float *)CPLMalloc(tx * ty * sizeof(float));
-    buffS = (float *)CPLMalloc(tx * ty * sizeof(float));
+	cout << "you can choose the way to create the picture" << endl;
+	cout << "1、 divided by line" << endl;
+	cout << "2、 divided by block" << endl;
+	cin >> choose;
+	if(choose == 1){
+	ty = imgYlen;
+	}else{
+	ty = 256;
+	}
+	buffR = (float *)CPLMalloc(tx * ty * sizeof(float));
+	buffG = (float *)CPLMalloc(tx * ty * sizeof(float));
+	buffB = (float *)CPLMalloc(tx * ty * sizeof(float));
+	buffP = (float *)CPLMalloc(tx * ty * sizeof(float));
+	buffH = (float *)CPLMalloc(tx * ty * sizeof(float));
+	buffS = (float *)CPLMalloc(tx * ty * sizeof(float));
 
-    //选择处理方式
-    if(choose == 1){
-        char* outPath = "Out_large1.tif";
-        outPic = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(outPath,imgXlen,imgYlen,bandNum,GDT_Byte,NULL);
-        dividedByLine(tx,ty,imgXlen,mulPic,panPic,outPic,buffR,buffG,buffB,buffP,buffH,buffS);
-    }else if(choose == 2){
-        char* outPath = "Out_large2.tif";
-        outPic = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(outPath,imgXlen,imgYlen,bandNum,GDT_Byte,NULL);
-        dividedByBlock(tx,ty,imgXlen,imgYlen,mulPic,panPic,outPic,buffR,buffG,buffB,buffP,buffH,buffS);
-    }else{
+	//选择处理方式
+	if(choose == 1){
+	char* outPath = "Out_large1.tif";
+	outPic = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(outPath,imgXlen,imgYlen,bandNum,GDT_Byte,NULL);
+	dividedByLine(tx,ty,imgXlen,mulPic,panPic,outPic,buffR,buffG,buffB,buffP,buffH,buffS);
+	}else if(choose == 2){
+	char* outPath = "Out_large2.tif";
+	outPic = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(outPath,imgXlen,imgYlen,bandNum,GDT_Byte,NULL);
+	dividedByBlock(tx,ty,imgXlen,imgYlen,mulPic,panPic,outPic,buffR,buffG,buffB,buffP,buffH,buffS);
+	}else{
 		cout << "your input is not right!" << endl;
 		return 0;
 	}
-    //释放缓存
-    CPLFree(buffR);
-    CPLFree(buffG);
-    CPLFree(buffB);
-    CPLFree(buffP);
-    CPLFree(buffH);
-    CPLFree(buffS);
-    //关闭数据集
-    GDALClose(panPic);
-    GDALClose(mulPic);
-    GDALClose(outPic);
+	//释放缓存
+	CPLFree(buffR);
+	CPLFree(buffG);
+	CPLFree(buffB);
+	CPLFree(buffP);
+	CPLFree(buffH);
+	CPLFree(buffS);
+	//关闭数据集
+	GDALClose(panPic);
+	GDALClose(mulPic);
+	GDALClose(outPic);
 
 	return 0;
 }
